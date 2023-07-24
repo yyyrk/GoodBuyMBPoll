924ba266ae5e35f7dc5a9faeb1114b838d296ce4
Yurik <78261534+yurjo7@users.noreply.github.com>
2023-07-24 19:54:07 +0200

    Greatest update main.py
    many simple changes

--- main.py
+++ main.py
@@ -3,88 +3,96 @@ from pyModbusTCP.client import ModbusClient
 import time
 import sys
 
-stat1 = 1
-stat2 = 1
-stat3 = 1
-stat4 = 1
-stat5 = 1
-stat6 = 1
-stat7 = 1
-stat8 = 1
+stat = [0,0,0,0,0,0,0,0]
+##stat1 = 1
+##stat2 = 1
+##stat3 = 1
+##stat4 = 1
+##stat5 = 1
+##stat6 = 1
+##stat7 = 1
+##stat8 = 1
 
-ishl1 = 1
-ishl2 = 1
-ishl3 = 1
-ishl4 = 1
-ishl5 = 1
-ishl6 = 1
-ishl7 = 1
-ishl8 = 1
+ishl = [0,0,0,0,0,0,0,0]
+##ishl1 = 1
+##ishl2 = 1
+##ishl3 = 1
+##ishl4 = 1
+##ishl5 = 1
+##ishl6 = 1
+##ishl7 = 1
+##ishl8 = 1
 
-comm1 = 1
-comm2 = 1
-comm3 = 1
-comm4 = 1
-comm5 = 1
-comm6 = 1
-comm7 = 1
-comm8 = 1
+comm = [0,0,0,0,0,0,0,0]
+##comm1 = 1
+##comm2 = 1
+##comm3 = 1
+##comm4 = 1
+##comm5 = 1
+##comm6 = 1
+##comm7 = 1
+##comm8 = 1
 
-strat1 = 1
-strat2 = 1
-strat3 = 1
-strat4 = 1
-strat5 = 1
-strat6 = 1
-strat7 = 1
-strat8 = 1
+strat = [0,0,0,0,0,0,0,0]
+##strat1 = 1
+##strat2 = 1
+##strat3 = 1
+##strat4 = 1
+##strat5 = 1
+##strat6 = 1
+##strat7 = 1
+##strat8 = 1
 
-porog11 = 1
-porog12 = 1
-porog13 = 1
-porog14 = 1
-porog15 = 1
-porog16 = 1
-porog17 = 1
-porog18 = 1
+porog1 = [0,0,0,0,0,0,0,0]
+##porog11 = 1
+##porog12 = 1
+##porog13 = 1
+##porog14 = 1
+##porog15 = 1
+##porog16 = 1
+##porog17 = 1
+##porog18 = 1
 
-porog21 = 1
-porog22 = 1
-porog23 = 1
-porog24 = 1
-porog25 = 1
-porog26 = 1
-porog27 = 1
-porog28 = 1
+porog2 = [0,0,0,0,0,0,0,0]
+##porog21 = 1
+##porog22 = 1
+##porog23 = 1
+##porog24 = 1
+##porog25 = 1
+##porog26 = 1
+##porog27 = 1
+##porog28 = 1
 
-porog31 = 1
-porog32 = 1
-porog33 = 1
-porog34 = 1
-porog35 = 1
-porog36 = 1
-porog37 = 1
-porog38 = 1
+porog3 = [0,0,0,0,0,0,0,0]
+##porog31 = 1
+##porog32 = 1
+##porog33 = 1
+##porog34 = 1
+##porog35 = 1
+##porog36 = 1
+##porog37 = 1
+##porog38 = 1
 
-porog41 = 1
-porog42 = 1
-porog43 = 1
-porog44 = 1
-porog45 = 1
-porog46 = 1
-porog47 = 1
-porog48 = 1
+porog4 = [0,0,0,0,0,0,0,0]
+##porog41 = 1
+##porog42 = 1
+##porog43 = 1
+##porog44 = 1
+##porog45 = 1
+##porog46 = 1
+##porog47 = 1
+##porog48 = 1
 
 sg.Input(default_text='Введите заменяемый тег', key='TAG')
 sg.theme('Default')             #Обьясление цветовой схемы интерфейса
-data = [['Текущие состояние', stat1, stat2, stat3, stat3, stat4, stat5, stat6, stat7, stat8],
-['Ток шлейфа(мкА)', ishl1, ishl2, ishl3, ishl4, ishl5, ishl6, ishl7, ishl8],
-['Команда', comm1, comm2, comm3, comm4, comm5, comm6, comm7, comm8],
-['Стратегия', strat1, strat2, strat3, strat4, strat5, strat6, strat7, strat8],
-['Порог 1', porog11, porog12, porog13, porog14, porog15, porog16, porog17, porog18],
-['Порог 2', porog21, porog22, porog23, porog24, porog25, porog26, porog27, porog28],
-['Порог 3', porog31, porog32, porog33, porog34, porog35, porog36, porog37, porog38],
-['Порог 4', porog41, porog42, porog43, porog44, porog45, porog46, porog47, porog48]]
+data = [['Текущие состояние', stat[0], stat[1], stat[2], stat[3], stat[4], stat[5], stat[6], stat[7]],
+['Ток шлейфа(мкА)', ishl[0], ishl[1], ishl[2], ishl[3], ishl[4], ishl[5], ishl[6], ishl[7]],
+['Команда', comm[0], comm[1], comm[2], comm[3], comm[4], comm[5], comm[6], comm[7]],
+['Стратегия', strat[0], strat[1], strat[2], strat[3], strat[4], strat[5], strat[6], strat[7]],
+['Порог 1', porog1[0], porog1[1], porog1[2], porog1[3], porog1[4], porog1[5], porog1[6], porog1[7]],
+['Порог 2', porog2[0], porog2[1], porog2[2], porog2[3], porog2[4], porog2[5], porog2[6], porog2[7]],
+['Порог 3', porog3[0], porog3[1], porog3[2], porog3[3], porog3[4], porog3[5], porog3[6], porog3[7]],
+['Порог 4', porog4[0], porog4[1], porog4[2], porog4[3], porog4[4], porog4[5], porog4[6], porog4[7]]]
 #['Таймер 1', timer11, timer12, timer13, timer14, timer15, timer16, timer17, timer18],
 #['Таймер 2', timer21, timer22, timer23, timer24, timer25, timer26, timer27, timer28],
 #['Таймер 3', timer31, timer32, timer33, timer34, timer35, timer36, timer37, timer38],
@@ -95,8 +103,9 @@ headings = ['', 'Шлейф 1', 'Шлейф 2', 'Шлейф 3', 'Шлейф 4', 
 layout = [          #Интерфейс
     [sg.Text('IP адрес устройства'), sg.Input(default_text='192.9.200.99', key='IP')],   #Строчка для ввода IP
     [sg.Text('Порт устройства'), sg.Input(default_text='502', key='port')],   #Строчка для ввода IP
-    [sg.Text('Таблица чтения')], #Сообщение
-    [sg.Table(values=data, headings=headings, max_col_width=35,
+    [sg.Text('Status: ready', key='out')], #Вывод статуса программы
+    [sg.Text('Таблица чтения'), sg.Button('Connect')], #Сообщение
+    [sg.Table(values=data, headings=headings, max_col_width=30,
                     # background_color='light blue',
                     auto_size_columns=True,
                     #display_row_numbers=True,
@@ -112,38 +121,37 @@ layout = [          #Интерфейс
     [sg.Text('Порог 2               '), sg.Input(key='p21', size=(8,1)), sg.Input(key='p22', size=(8,1)), sg.Input(key='p23', size=(8,1)), sg.Input(key='p24', size=(8,1)), sg.Input(key='p25', size=(8,1)), sg.Input(key='p26', size=(8,1)), sg.Input(key='p27', size=(8,1)), sg.Input(key='p28', size=(8,1))],
     [sg.Text('Порог 3               '), sg.Input(key='p31', size=(8,1)), sg.Input(key='p32', size=(8,1)), sg.Input(key='p33', size=(8,1)), sg.Input(key='p34', size=(8,1)), sg.Input(key='p35', size=(8,1)), sg.Input(key='p36', size=(8,1)), sg.Input(key='p37', size=(8,1)), sg.Input(key='p38', size=(8,1))],
     [sg.Text('Порог 4               '), sg.Input(key='p41', size=(8,1)), sg.Input(key='p42', size=(8,1)), sg.Input(key='p43', size=(8,1)), sg.Input(key='p44', size=(8,1)), sg.Input(key='p45', size=(8,1)), sg.Input(key='p46', size=(8,1)), sg.Input(key='p47', size=(8,1)), sg.Input(key='p48', size=(8,1))],
-    
-    [sg.Text('Status: ready', key='out')], #Вывод статуса программы
-    [sg.Button('Connect')]   #Кнопка
+    [sg.Button('Writh')]   #Кнопка
 ]
 window = sg.Window('GoodBuyBMPull', layout, icon=r'C:/Users/rozze/OneDrive/Рабочий стол/Feels/Replicator.ico') #Обдьявление окна интерфейса
 
-def main():
-    ip_address = values[127.000.000.001]
-    c = ModbusClient()
-    if not c.host(ip_address):
-        window['out'].update(f'Status: IP error')
-    if not c.port(502):
-        window['out'].update(f'Status: port error')
+def Connect():
+    ip_address = values['IP']
+    ##ip_address = int(str_ip_address) 
+    str_port = values['port']
+    port = int(str_port) 
+    MB = ModbusClient(host=ip_address, port=port)
+    MB.open()
     while True:
-        if c.is_open():
-            rr = c.read_discrete_inputs(0, 2)
+        if MB.is_open():
+            window['out'].update(f'Status: Connect is done!')
+            rr = MB.read_holding_registers(3, 8)
             # [False, True]
             if not rr[0] and rr[1]:
                 print ('work')
-                sys.exit(0)
+               ## sys.exit(0)
             # [True, True]
             elif rr[0] and rr[1]:
                 print ('not work')
-                sys.exit(1)
+               ## sys.exit(1)
             print ('-')
-            sys.exit(-1)
+           ## sys.exit(-1)
         else:
-            c.open()
+            window['out'].update(f'Status: Connect is fail!')
             
 while True:
     event, values = window.read()
     if event is None or event == sg.WIN_CLOSED:
         break
     if event == 'Connect':
-        main()
+        Connect()
